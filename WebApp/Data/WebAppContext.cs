3213dde548307fa0c3f4bd90a43b0a8038c8d003
Silvio Jose de Souza <silvio.j.souza@gmail.com>
2019-02-06 15:12:18 -0200

    Add project files.

---
+++ WebApp/Data/WebAppContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApp.Models
+{
+    public class WebAppContext : DbContext
+    {
+        public WebAppContext (DbContextOptions<WebAppContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebApp.Models.Cliente> Cliente { get; set; }
+    }
+}
---
+++ WebApp/Models/Cliente.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.Models
+{
+    [Table("Cliente")]
+    public class Cliente
+    {
+        [Key]
+        public int Codigo { get; set; }
+        [Required(ErrorMessage = "Informe o nome do cliente")]
+        public string Nome { get; set; }
+        [Required(ErrorMessage = "Informe o e-mail do cliente")]
+        public string Email { get; set; }
+
+    }
+}
